b8dad63538e7e48f26464429037185b2c3f40092
Adam Shire <shirea@mit.edu>
2019-12-16 09:56:41 -0500

    day 4 star 2

--- day4/day4.py
+++ day4/day4.py
@@ -1,18 +1,25 @@
 # for each number, turn it into a list
 # check each list against criteria
 
-in_order = []
+import re
+from collections import Counter
+passwords = []
 
 print(f"starting count: {len(range(156218,652527))}")
 
 for number in range(156218, 652527):
     # print(number)
     res = [int(x) for x in str(number)]
+    # if there are no duplicates move on
     if len(res) == len(set(res)):
         continue
+    # check to see if the list is in order
     if all(res[i] <= res[i+1] for i in range(len(res)-1)):
-        # add to list of things that are in order
-        in_order.append(res)
-        # print(res)
+        # add it to the list of possible passwords 
+        histo = dict((x, res.count(x)) for x in res)
+        if 2 in histo.values():
+            passwords.append(res)
+            print(res)
 
-print(f"ending count:{len(in_order)}")
+
+print(f"ending count:{len(passwords)}")
